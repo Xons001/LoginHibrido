f7fdbb57d17a816eb03ccf173ee0dde4d9d41a11
Sean <sean.saez.f@gmail.com>
2019-02-11 20:28:50 +0100

    Acabado el params

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let express = require('express');
 var bodyParser = require('body-parser')
 let app = express();
-var usuarios = [{name : 'Roger', pass : "1234"},
-                {name : 'Sean', pass : "321"},
-               {name : 'Alberto', pass : "6969"},];
+var usuarios = [{name : 'roger', pass : "1234"},
+                {name : 'sean', pass : "321"},
+               {name : 'alberto', pass : "6969"},];
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'ejs');
 
@@ -14,13 +14,24 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
 
   for (var i = 0; i < usuarios.length; i++) {
-    if(usuarios[i]['name'] == req.body.nombre &&usuarios[i]['pass'] == req.body.pass){
+    if(usuarios[i]['name'] == req.body.nombre && usuarios[i]['pass'] == req.body.pass){
       console.log("usuario correcto!!!")
       res.send('Usuario Correcto!  Nombre: ' + req.body.nombre + ' => Password: ' + req.body.pass);
       return;
     }
   }
   res.send("usuario incorrecto");
+});
+
+app.get('/api/login/:user/:pass', function (req, res) {
+
+  for (var i = 0; i < usuarios.length; i++) {
+    if(usuarios[i]['name'] == req.params.user && usuarios[i]['pass'] == req.params.pass){
+      res.send('Usuario Correcto!  Nombre: ' + req.params.user + ' => Password: ' + req.params.pass);
+      return;
+    }
+    res.send("usuario incorrecto");
+  }
 });
 
 app.listen(3000, function() {
